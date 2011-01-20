6d126731b34d599fd6c5420b13cb8f466db5dc7c
Jeff Bilyk <jbilyk@gmail.com>
2011-01-19 21:53:05 -0500

    amaaps: added code to insert results from openaudit into nagiosql.tbl_host

--- amaaps.pl
+++ amaaps.pl
@@ -1,7 +1,7 @@
 #!perl -w
 
 # Author: Jeff Bilyk
-# Date: October 31, 2010
+# Date: January 18, 2011
 # Purpose:        To take entries from the database used by OpenAudit and
 #                 insert them into the database used by NagioSQL so that
 #                 new hosts will be monitored automatically by Nagios.
@@ -11,7 +11,8 @@
 # Implementation: Two MySQL databases are accessed: OpenAudit and 
 #                 db_nagiosql_v3.  First, servers that are running Microsoft
 #                 Windows Server operating systems are selected and added
-#                 to the windowsservers hostgroup in Nagios.  Then, the following
+#                 to the tbl_host table.  Then they're added to the windowsservers
+#		  hostgroup in Nagios.  Then, the following
 #                 software is detected and added to a hostgroup for each:
 #                 Exchange, SQL Server, Lotus Domino.
 
@@ -24,6 +25,7 @@ my $port = "3306";
 my $tablename = "tbl_host";
 my $user = "jeff";
 my $password = "testing";
+my @tempresult;
 
 my $dsn = "dbi:mysql:$database:$host:$port";
 
@@ -31,55 +33,15 @@ my $DBIconnect = DBI->connect($dsn,$user,$password);
 
 # Exchange servers
 
-my $myquery = "select distinct system.system_name,system.net_ip_address from system,software where software_name like '%icrosoft%xchange' and software.software_uuid = system.system_uuid;";
-
-my $runit = $DBIconnect->prepare($myquery);
-
-$runit->execute();
-
-printf "Exchange servers:\n";
-
-while (my @results = $runit->fetchrow_array()) {
-	printf $results[0];
-	printf "  ";
-	printf $results[1];
-	printf "\n";
-}
+#my $myquery = "select distinct system.system_name,system.net_ip_address from system,software where software_name like '%icrosoft%xchange' and software.software_uuid = system.system_uuid;";
 
 # SQL Servers
 
-printf "\nSQL Servers\n\n";
-
-$myquery = "select distinct system.system_name,system.net_ip_address from system,software where software_name like '%icrosoft%SQL%erver%20%' and software.software_uuid = system.system_uuid;";
-
-$runit = $DBIconnect->prepare($myquery);
-
-$runit->execute();
-
-while (@results = $runit->fetchrow_array()) {
-	printf $results[0];
-	printf "  ";
-	printf $results[1];
-	printf "\n";
-}
+#$myquery = "select distinct system.system_name,system.net_ip_address from system,software where software_name like '%icrosoft%SQL%erver%20%' and software.software_uuid = system.system_uuid;";
 
 # Lotus Domino servers
 
-printf "\nDomino Servers\n\n";
-
-$myquery = "select distinct system.system_name,system.net_ip_address from system,software where software_name like '%otus%omino%' and software.software_uuid = system.system_uuid;";
-
-$runit = $DBIconnect->prepare($myquery);
-
-$runit->execute();
-
-while (@results = $runit->fetchrow_array()) {
-	printf $results[0];
-	printf "  ";
-	printf $results[1];
-	printf "\n";
-}
-
+#$myquery = "select distinct system.system_name,system.net_ip_address from system,software where software_name like '%otus%omino%' and software.software_uuid = system.system_uuid;";
 
 # Windows Servers
 
@@ -88,14 +50,17 @@ printf "\nWindows Servers\n\n";
 $myquery = "select distinct system.system_name,system.net_ip_address from system where system_os_name like '%icrosoft%erver%'";
 
 $runit = $DBIconnect->prepare($myquery);
-
 $runit->execute();
 
 while (@results = $runit->fetchrow_array()) {
-        printf $results[0];
-        printf "  ";
-	printf $results[1];
-	printf "\n";
+	$results[1] =~ s/^0//;
+	$results[1] =~ s/\.0/\./;
+	$results[1] =~ s/\.0/\./;
+	$results[1] =~ s/\.0/\./;
+$myquery = "insert into db_nagiosql_v3.tbl_host (host_name, alias, address, use_template) VALUES ('$results[0]', '$results[0]', '$results[1]', '1')";
+	printf "Query: $myquery\n";
+	$runit1 = $DBIconnect->prepare($myquery);
+	$runit1->execute();
 }
 
 
